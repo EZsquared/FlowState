318b63cf611f6f16c471543d174257c152d038c5
Eric Cannon <65966956+EZsquared@users.noreply.github.com>
2021-03-10 17:33:13 -0500

    Added Howler functionality to play music, added Node server to eventually serve mp3's to front-end.

--- src/Components/NewPlayer/NewPlayer.js
+++ src/Components/NewPlayer/NewPlayer.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './NewPlayer.css';
+import data from '../tempInfo.json';
+import ReactHowler from 'react-howler';
+import raf from 'raf';
+import song from '../songs/EG.mp3'
 
+const songs = data.songs;
 
+const getData = '127.0.0.1:4000'
 
 function handleOption (opId) {
       const option = document.getElementById(opId);
@@ -17,19 +23,132 @@ function handleOption (opId) {
 function handlePlay (opId) {
     const option = document.getElementById(opId);
     if (option.value === true) {
-      option.classList.remove('icon-play4');
-      option.classList.add('icon-pause3');
+      option.classList.remove('icon-pause3');
+      option.classList.add('icon-play4');
       option.value = false;
       } else {
-        option.classList.remove('icon-pause3');
-        option.classList.add('icon-play4');
+        option.classList.remove('icon-play4');
+        option.classList.add('icon-pause3');
         option.value = true;
       }
 }
   
-function NewPlayer() {
-
-    return (
+class NewPlayer extends React.Component{
+    constructor (props) {
+        super(props)
+    
+        this.state = {
+          status: 'ready',
+          source: song,
+          playing: false,
+          loaded: false,
+          loop: false,
+          mute: false,
+          volume: 1.0,
+          seek: 0.0,
+          isSeeking: false
+        }
+        this.handleToggle = this.handleToggle.bind(this)
+        this.handleOnLoad = this.handleOnLoad.bind(this)
+        this.handleOnEnd = this.handleOnEnd.bind(this)
+        this.handleOnPlay = this.handleOnPlay.bind(this)
+        this.handleStop = this.handleStop.bind(this)
+        this.renderSeekPos = this.renderSeekPos.bind(this)
+        this.handleLoopToggle = this.handleLoopToggle.bind(this)
+        this.handleMuteToggle = this.handleMuteToggle.bind(this)
+        this.handleMouseDownSeek = this.handleMouseDownSeek.bind(this)
+        this.handleMouseUpSeek = this.handleMouseUpSeek.bind(this)
+        this.handleSeekingChange = this.handleSeekingChange.bind(this)
+      }
+    
+      componentWillUnmount () {
+        this.clearRAF()
+      }
+    
+      handleToggle () {
+        this.setState({
+          playing: !this.state.playing
+        })
+      }
+    
+      handleOnLoad () {
+        this.setState({
+          loaded: true,
+          duration: this.player.duration()
+        })
+      }
+    
+      handleOnPlay () {
+        this.setState({
+          playing: true
+        })
+        this.renderSeekPos()
+      }
+    
+      handleOnEnd () {
+        this.setState({
+          playing: false
+        })
+        this.clearRAF()
+      }
+    
+      handleStop () {
+        this.player.stop()
+        this.setState({
+          playing: false // Need to update our local state so we don't immediately invoke autoplay
+        })
+        this.renderSeekPos()
+      }
+    
+      handleLoopToggle () {
+        this.setState({
+          loop: !this.state.loop
+        })
+      }
+    
+      handleMuteToggle () {
+        this.setState({
+          mute: !this.state.mute
+        })
+      }
+    
+      handleMouseDownSeek () {
+        this.setState({
+          isSeeking: true
+        })
+      }
+    
+      handleMouseUpSeek (e) {
+        this.setState({
+          isSeeking: false
+        })
+    
+        this.player.seek(e.target.value)
+      }
+    
+      handleSeekingChange (e) {
+        this.setState({
+          seek: parseFloat(e.target.value)
+        })
+      }
+    
+      renderSeekPos () {
+        if (!this.state.isSeeking) {
+          this.setState({
+            seek: this.player.seek()
+          })
+        }
+        if (this.state.playing) {
+          this._raf = raf(this.renderSeekPos)
+        }
+      }
+    
+      clearRAF () {
+        raf.cancel(this._raf)
+      }
+    
+    render(){
+        return (
             <div className="Player lcd-panel">
                 <div className="info-header">
                     <div className="noti-cent-item others icon-cog2"></div>
@@ -39,17 +158,20 @@ function NewPlayer() {
                     <div className="noti-cent-item others icon-speech-bubble"></div>
                 </div>
                 <div>
-                    <div className="action">Now playing</div>
+                    <div className="action">{
+                    (this.state.loaded) ? this.state.status : 'Loading'
+                    }</div>
                     <div className="TrackInformation">
                         <div className="Name">Awesome Song</div>      
                     </div>
                 </div>
                 <div className="timebar">
-                    
-                    <div className="artist-album">
-                        <div className="Artist">Various Artists</div>
-                        <div className="Album">Totally Cool Album</div>
-                    </div>
+                  {(this.state.seek / 60).toFixed(2)}
+                  <div className="artist-album">
+                      <div className="Artist">Various Artists</div>
+                      <div className="Album">Totally Cool Album</div>
+                  </div>
+                  {(this.state.duration) ? ((this.state.duration / 60) - (this.state.seek / 60)).toFixed(2) : ''}
                 </div>
                 <div className="controls-wrapper">
                     <div className="Controls">
@@ -63,22 +185,42 @@ function NewPlayer() {
                         <div  className="others"></div>
                         <div className="Button icon-skip-back others"></div>
                         <div  className="Button icon-rewind others"></div>
-                        <div className="Button icon-play4" id='play' onClick={() =>{handlePlay('play')}}></div>
+                        <div className="Button icon-play4" value='false' id='play' onClick={() =>{handlePlay('play'); this.handleToggle()}}></div>
                         <div  className="Button icon-fast-forward others"></div>
                         <div  className="Button icon-skip-forward others"></div>
                         <div className="others"></div>
-                        <div  className="others"></div>
-                        <div  className="Button icon-heart others Option" id="heart" onClick={() =>{handleOption('heart')}}></div>
+                        <div className="others"></div>
+                        <div className="Button icon-heart others Option" id="heart" onClick={() =>{handleOption('heart')}}></div>
                     </div>
                 </div>
-                <div className="Scrubber">
-                    <div className="Scrubber-Progress"></div>
-                </div>
-                <audio id="audio">
-                    {/* <source src=/> */}
-                </audio>            
+                <ReactHowler
+                    src={this.state.source}
+                    playing={this.state.playing}
+                    onLoad={this.handleOnLoad}
+                    onPlay={this.handleOnPlay}
+                    onEnd={this.handleOnEnd}
+                    loop={this.state.loop}
+                    mute={this.state.mute}
+                    volume={this.state.volume}
+                    ref={(ref) => (this.player = ref)}
+                    />
+
+                    <input
+                      type='range'
+                      min='0'
+                      max={this.state.duration ? this.state.duration.toFixed(2) : 0}
+                      step='.001'
+                      value={this.state.seek}
+                      onChange={this.handleSeekingChange}
+                      onMouseDown={this.handleMouseDownSeek}
+                      onMouseUp={this.handleMouseUpSeek}
+                    />
+
+                
             </div>
     )
+    }
+    
 }
 
 export default NewPlayer
